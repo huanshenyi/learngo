81efa0791948a46e16d7d13c41fdc788e2522d34
huanshenyi <txy1226052@gmail.com>
2019-11-03 17:20:13 +0900

    2-5over

---
+++ loop.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func convertToBin(n int) string {
+    result := ""
+    for ; n>0;n/=2{
+    	lsb := n%2
+    	result = strconv.FormatInt(int64(lsb),10) + result
+	}
+    return result
+}
+
+func printFile(filename string)  {
+	file,err := os.Open(filename)
+	if err !=nil{
+		panic(err)
+	}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan(){
+		fmt.Println(scanner.Text())
+	}
+}
+
+func readFile(filename string)  {
+	file,err := os.Open(filename)
+	if err != nil{
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	read := bufio.NewReader(file)
+	b := make([]byte,1024)
+	n,err := read.Read(b)
+	if err != nil{
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(n) //26
+	fmt.Println(string(b[:n]))
+	/*
+	abcde
+	12345
+	hello
+	again
+	*/
+
+}
+
+func main()  {
+	//fmt.Println(
+	//	convertToBin(5), //101
+	//	convertToBin(13), // 1101
+	//	)
+	//fmt.Println(strconv.FormatUint(5,2))
+	//printFile("abc.txt")
+	readFile("abc.txt")
+}
