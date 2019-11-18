1a90312670f17cf03594ed6b8c27131b0ec7aeb0
huanshenyi <txy1226052@gmail.com>
2019-11-18 22:21:22 +0900

    3-3 over

--- slices.go
+++ slices.go
@@ -37,4 +37,11 @@ func main() {
 	fmt.Printf("s1=%v, len(s1)=%d, cap(s1)=%d\n", s1, len(s1), cap(s1)) // 2, 3, 4, 5
 	fmt.Printf("s1=%v, len(s1)=%d, cap(s1)=%d", s2, len(s2), cap(s2))   // 5, 6
 
+	s3 := append(s2, 10)
+	s4 := append(s3, 11)
+	s5 := append(s4, 12)
+	fmt.Println("s3, s4, s5=", s3, s4, s5)
+	// s4 and s5 no longer view arr.
+	fmt.Println("arr =", arr)
+
 }
