614f007ee88a6630fa9adfbd7e88688e7bb35f31
huanshenyi <txy1226052@gmail.com>
2019-11-25 21:44:12 +0900

    4-1 over

--- tree/node.go
+++ tree/node.go
@@ -7,22 +7,40 @@ type treeNode struct {
 	left, right *treeNode
 }
 
+func (node *treeNode) print() {
+	fmt.Println(node.value)
+}
+
+func (node *treeNode) setValue(value int) {
+	if node == nil {
+		fmt.Println("Setting value to nil node.Ignored")
+		return
+	}
+	node.value = value
+}
+
 func createNode(value int) *treeNode {
 	return &treeNode{value: value}
+}
+
+func (node *treeNode) traverse() {
+	if node == nil {
+		return
+	}
+	node.left.traverse()
+	node.print()
+	node.right.traverse()
 }
 
 func main() {
 	var root treeNode
 	root = treeNode{value: 3}
 	root.left = &treeNode{}
-	root.right = &treeNode{5, nil, nil}
+	root.right = &treeNode{5, new(treeNode), new(treeNode)}
 	root.right.right = new(treeNode)
-	fmt.Printf("%v\n", root) //{  3, left:*{0,nil,nil},right:{5,nil,{0,nil,nil}}
+	root.left.right = createNode(2)
 
-	nodes := []treeNode{
-		{value: 3},
-		{},
-		{6, nil, &root},
-	}
-	fmt.Println(nodes)
+	root.right.left.setValue(4)
+
+	root.traverse()
 }
