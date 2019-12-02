828007e2fe71121f7726c4384453d71eeb9959c5
huanshenyi <txy1226052>
2019-12-02 12:00:03 +0900

    6-5 over

--- retriever/main.go
+++ retriever/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"main/retriever/mock"
 	real2 "main/retriever/real"
 	"time"
@@ -11,13 +12,41 @@ type Retriever interface {
 	Get(url string) string
 }
 
+type Poster interface {
+	Post(url string,
+		form map[string]string) string
+}
+
+const url = "https://www.imooc.com"
+
 func download(r Retriever) string {
-	return r.Get("https://www.imooc.com")
+	return r.Get(url)
+}
+
+func Post(poster Poster) {
+	poster.Post(url, map[string]string{
+		"name":   "ccmouse",
+		"course": "golang",
+	})
+}
+
+type RetrieverPoster interface {
+	Retriever
+	Poster
+}
+
+func session(s RetrieverPoster) string {
+	s.Post(url, map[string]string{
+		"contents": "another faked imooc.com",
+	})
+	return s.Get(url)
 }
 
 func main() {
+	io.ReadCloser()
 	var r Retriever
-	r = mock.Retriever{"mock imooc"}
+	retriever := mock.Retriever{"this is a fack imooc.com"}
+	r = retriever
 	inspect(r)
 	r = &real2.Retriever{
 		UserAgent: "Mozilla/5.0",
@@ -33,6 +62,8 @@ func main() {
 	}
 
 	//fmt.Println(download(r))
+	fmt.Println("Try a session")
+	fmt.Println(session(&retriever))
 }
 
 func inspect(r Retriever) {
