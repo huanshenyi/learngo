bdbedab1f2a7bfdf4ed2663ea163f7de966b7f94
huanshenyi <txy1226052@gmail.com>
2020-01-01 18:18:59 +0900

    16-3 over

--- crawler/main.go
+++ crawler/main.go
@@ -3,13 +3,21 @@ package main
 import (
 	"main/crawler/engine"
 	"main/crawler/paiza/parser"
+	"main/crawler/scheduler"
 )
 
 func main() {
 
-	engine.Run(engine.Request{
+	//engine.SimpleEngine.Run(engine.Request{
+	//	Url:        "https://paiza.jp/career/job_offers/dev_language/Kotlin",
+	//	ParserFunc: parser.ParseLanguageList,
+	//})
+	e := engine.ConcurrentEngine{
+		Scheduler:   &scheduler.SimpleScheduler{},
+		WorkerCount: 10,
+	}
+	e.Run(engine.Request{
 		Url:        "https://paiza.jp/career/job_offers/dev_language/Kotlin",
 		ParserFunc: parser.ParseLanguageList,
 	})
-
 }
